f56bc907b8aeff3ef360d24d57ae2a55a7550262
atharv3731 <atharvjadhav7526@gmail.com>
2026-08-23 20:51:52 +0530

    Function

---
+++ C/FunctionDemo1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    // Local Vari
+    int value1 = 10;
+    int value2 = 11;
+    int ans = 0;
+    ans = value1 + value2;
+    printf("Addition is :%d\n",ans);
+
+     return 0;
+
+}
---
+++ C/FunctionDemo3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Global Variable
+    int value1 = 10;
+    int value2 = 11;
+    int ans = 0;
+int main(){
+    ans = value1 + value2;
+     printf("Addition is :%f\n",ans);
+
+     return 0;
+
+}
