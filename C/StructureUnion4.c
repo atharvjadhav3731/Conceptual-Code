12d2c7eb3fb8354257a3ced325c785eb5c9f3a7c
atharv3731 <atharvjadhav7526@gmail.com>
2026-08-24 15:17:47 +0530

    Structure&Union

---
+++ C/StructureUnion4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#pragma pack(1)
+struct Demo
+{
+    int i;
+    float f;
+    struct hello 
+    {
+        int no;
+        float marks;
+
+    };
+};
+int main()
+{
+    struct Demo dobj;
+    printf("%d\n",sizeof(dobj));
+    
+    return 0;
+}
