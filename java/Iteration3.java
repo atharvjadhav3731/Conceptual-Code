2f13310cafc15a211d5c927596eb0898700ff2ed
atharv3731 <atharvjadhav7526@gmail.com>
2026-09-19 11:24:00 +0530

    Iteration

---
+++ java/Iteration3.java
@@ -0,0 +1,17 @@
+class Iteration3 {
+    
+    public static void main(String[] args)
+    {
+        int i =0;
+        i = 1;   // 1
+        //.    2
+        while(i<=4)
+        {            
+            System.out.println("Jay Ganesh..."); // 4
+            i++;                                   // 3
+        }
+        
+    
+    }
+}
+
